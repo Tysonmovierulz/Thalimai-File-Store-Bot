f0291550960956c008c644cc9c122768430a372e
Tysonmovierulz <100193933+Tysonmovierulz@users.noreply.github.com>
2022-05-12 10:42:27 +0530

    Update config.php

--- web/config.php
+++ web/config.php
@@ -22,11 +22,11 @@ else {
 }
 
 $GLOBALS["START_MESSAGE"] = <<<EOM
-🤩 𝐓𝐡𝐚𝐧𝐤 𝐲𝐨𝐮 𝐟𝐨𝐫 𝐮𝐬𝐢𝐧𝐠 @Bruce_FilesBot<a href="https://t.me/Brucebots"></a>
+𝐓𝐡𝐚𝐧𝐤 𝐘𝐨𝐮 𝐅𝐨𝐫 𝐉𝐨𝐢𝐧𝐢𝐧𝐠 𝐓𝐇𝐀𝐋𝐀𝐈𝐕𝐀 𝐃𝐔𝐁𝐁𝐄𝐃 𝐌𝐎𝐕𝐈𝐄𝐒 🤩<a href="https://t.me/Brucebots"></a>
 
-<u><b> 𝐘𝐨𝐮 𝐂𝐚𝐧 𝐅𝐨𝐫𝐰𝐚𝐫𝐝 𝐌𝐞 𝐀𝐧𝐲 𝐌𝐞𝐝𝐢𝐚 𝐌𝐞𝐬𝐬𝐚𝐠𝐞 </b></u>, && <b><i> 𝐈 𝐌𝐢𝐠𝐡𝐭 𝐇𝐞𝐥𝐩 𝐘𝐨𝐮 𝐓𝐨 𝐂𝐫𝐞𝐚𝐭𝐞 𝐀 𝐏𝐮𝐛𝐥𝐢𝐜 𝐋𝐢𝐧𝐤</i></b>.
+<u><b> 𝐇𝐞𝐥𝐥𝐨 𝐈 𝐚𝐦 𝐅𝐢𝐥𝐞 𝐒𝐭𝐨𝐫𝐞 𝐁𝐨𝐭 </b></u>,<b><i>𝐓𝐡𝐢𝐬 𝐁𝐨𝐭 𝐈𝐬 𝐒𝐩𝐞𝐜𝐢𝐚𝐥𝐥𝐲 𝐌𝐚𝐝𝐞 𝐅𝐨𝐫 𝐓𝐇𝐀𝐋𝐀𝐈𝐕𝐀 𝐌𝐎𝐕𝐈𝐄𝐒.</i></b>.
 
-😉 𝐃𝐨 𝐓𝐡𝐚𝐧𝐤𝐬 𝐓𝐨 @bruce_3_33 𝐈𝐟 𝐘𝐨𝐮  💕 𝐔𝐬𝐢𝐧𝐠 𝐓𝐡𝐢𝐬 𝐁𝐨𝐭.
+𝐂𝐥𝐢𝐜𝐤 𝐎𝐧 𝐒𝐭𝐚𝐫𝐭 𝐓𝐨 𝐆𝐞𝐭 𝐌𝐨𝐯𝐢𝐞.
 EOM;
-$GLOBALS["CHECKING_MESSAGE"] = "⚡️";
+$GLOBALS["CHECKING_MESSAGE"] = "";
 require_once __DIR__ . "/../vendor/autoload.php";
